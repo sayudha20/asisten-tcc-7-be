dcc4f34acddfe2c1c3b6d60f4c1e6f96be36759b
plirapli <rafliyulistiawan@gmail.com>
2025-05-08 09:21:18 +0700

    fix: user tb

--- models/UserModel.js
+++ models/UserModel.js
@@ -10,7 +10,8 @@ const User = db.define(
     gender: Sequelize.STRING,
     password: Sequelize.STRING,
     refresh_token: Sequelize.TEXT,
-  }
+  },
+  { freezeTableName: true }
 );
 
 db.sync().then(() => console.log("Database synced"));
